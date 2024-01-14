4402aa2257947c30bf656404bc9f2959d98925b2
Shv <arhangelalen.2019@gmail.com>
2024-01-14 05:48:33 +0200

    Опис змін

---
+++ dataroot/localcache/bootstrap.php
@@ -0,0 +1,8 @@
+<?php
+// ********** This file is generated DO NOT EDIT **********
+$CFG->siteidentifier = 'h4T43wUxUGSG6aRBSOrOC7Y3s4IVGENalocalhost';
+$CFG->bootstraphash = 'cc3d6bbb74a639212060212a4178caaf';
+// Only if the file is not stale and has not been defined.
+if ($CFG->bootstraphash === hash_local_config_cache() && !defined('SYSCONTEXTID')) {
+    define('SYSCONTEXTID', 1);
+}
